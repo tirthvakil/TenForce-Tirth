a64391268a3d4442fecb864c52008e823a5f9d6b
tirthvakil <tirthvakil36@gmail.com>
2022-09-16 23:01:41 +0530

    Tenforcetirth

---
+++ Test-Taste-Console-Application/Domain/Objects/Moon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Moon
+    {
+        public string Id { get; set; }
+        public decimal MassValue { get; set; }
+        public decimal MassExponent { get; set; }
+        public decimal gravity { get; set; }
+
+        public Moon(MoonDto moonDto)
+        {
+            Id = moonDto.Id;
+            MassValue = moonDto.MassValue;
+            MassExponent = moonDto.MassExponent;
+            gravity = moonDto.gravity;   // set gravity to each moon from MoonDto
+        }
+
+        
+    }
+}
